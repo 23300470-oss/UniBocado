04b4ffb94301609dd1c28b9c192c97efc72e684e
“Brandon_Uriel_Calixto” <brandonurielcalixtog@gmail.com>
2026-07-16 22:51:27 -0600

    fix: increase Express request body limit to 50mb to support Base64 images

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(__dirname));
 
 let supabase;
